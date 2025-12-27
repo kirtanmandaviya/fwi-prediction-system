55aed3ab68142215a983feea88b22b36bbf92990
MRKP3110 <161864051+MRKP3110@users.noreply.github.com>
2025-12-27 12:24:25 +0530

    Initial commit

--- application.py
+++ application.py
@@ -1,23 +1,21 @@
 from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 import pickle
+import os
 
 application = Flask(__name__)
 app = application
 
-## Import Ridge regressor and Standard scaler pickle
-
+# Load pickled model and scaler
 ridge_model = pickle.load(open('models/ridge.pkl', 'rb'))
 standard_scaler = pickle.load(open('models/scaler.pkl', 'rb'))
-
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/predictdata",methods = ['GET','POST'])
+@app.route("/predictdata", methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'POST':
         Temperature = float(request.form.get('Temperature'))
@@ -29,14 +27,14 @@ def predict_datapoint():
         ISI = float(request.form.get('ISI'))
         Classes = float(request.form.get('Classes'))
         Region = float(request.form.get('Region'))
-        
+
         new_scaled_data = standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
-        
         result = ridge_model.predict(new_scaled_data)
-        print(result)
-        return render_template('home.html',results=result[0])
+
+        return render_template('home.html', results=result[0])
     else:
         return render_template('home.html')
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port)
